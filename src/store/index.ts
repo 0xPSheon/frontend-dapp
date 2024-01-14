a21eeacb89e11229cedf6345e859976ed3c3c7f4
PSheon <pauljiang61020@gmail.com>
2024-01-14 22:31:05 +0800

    feat: added store base.

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,15 @@
 // ** Toolkit imports
 import { configureStore } from '@reduxjs/toolkit'
 
+// ** Reducers
+import dialog from 'src/store/dialog'
+
 // ** Api
 import { roleAndPermissionApi } from 'src/store/api/roleAndPermission'
 
 export const store = configureStore({
   reducer: {
+    dialog,
     [roleAndPermissionApi.reducerPath]: roleAndPermissionApi.reducer
   },
   middleware: getDefaultMiddleware =>
